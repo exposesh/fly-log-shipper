91773beb8708a9e2cf0c586cff819f58a19b9f1f
Gaëtan L. H.-F <81755261+gaetanlhf@users.noreply.github.com>
2023-11-24 00:05:35 +0100

    Update start-fly-log-transporter.sh

--- start-fly-log-transporter.sh
+++ start-fly-log-transporter.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+mkdir -p /var/secrets/
+echo "$GOOGLE_APPLICATION_CREDENTIALS" > /var/secrets/GOOGLE_APPLICATION_CREDENTIALS.json
 template() { eval $'cat <<_EOF\n'"$(awk '1;END{print"_EOF"}')"; }
 sponge() { cat <<<"$(cat)" >"$1"; }
 filter() { for i in "$@"; do template <"$i" | sponge "$i" || rm "$i"; done; }
